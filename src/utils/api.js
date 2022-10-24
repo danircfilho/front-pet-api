d5619a94f69691e1cafe3bfbc9ce51fd0bbcb19c
Daniel Rodrigues de Carvalho Filho <danircfilho@hotmail.com>
2022-10-23 22:31:28 -0300

    .env no axios - acertos

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 //Configuração para chamar a API em toda a aplicação
 import axios from "axios";
 
-const API = process.env.REACT_APP_API
+const API = `${process.env.REACT_APP_API}`
 
 export default axios.create({
   baseURL: API
