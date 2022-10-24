660f40d6e8dbab004ac85703b885a15c898f9dd5
Daniel Rodrigues de Carvalho Filho <danircfilho@hotmail.com>
2022-10-23 22:35:47 -0300

    axios = API string

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,6 @@
 //Configuração para chamar a API em toda a aplicação
 import axios from "axios";
 
-const API = `${process.env.REACT_APP_API}`
-
 export default axios.create({
-  baseURL: API
+  baseURL: 'https://node-api-01.onrender.com'
 })
